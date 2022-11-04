d5844d8052859da87c3e60bb16fbecfc33698765
Fernando Mirabile <fernando.mirabile@split.io>
2022-11-04 13:42:09 -0300

    Adding HTTPS and fixing an error

--- src/api/middleware/logHandler.ts
+++ src/api/middleware/logHandler.ts
@@ -1,6 +1,6 @@
-import express from 'express'
-import { AppDataSource } from '../../config/database'
-import { Log } from '../../model/models'
+import express from "express";
+import { AppDataSource } from "../../config/database";
+import { Log } from "../../model/models";
 
 export const logHandler = [
   async (
@@ -8,33 +8,31 @@ export const logHandler = [
     res: express.Response,
     next: express.NextFunction
   ) => {
-    
-    let status
-    let body= {}
+    let status = 200;
+    let body = {};
 
     let logRepository = AppDataSource.getRepository(Log);
-    let log = new Log()
-    log.request = req.body
-    log.status = status
-    log.url = req.url
-    log.verb = "@"+req.method
-    if((req as any).user){
-      log.userId = (req as any).user.id
+    let log = new Log();
+    log.request = req.body;
+    log.status = status;
+    log.url = req.url;
+    log.verb = "@" + req.method;
+    if ((req as any).user) {
+      log.userId = (req as any).user.id;
     }
-    if((res as any).response && (res as any).response.body){
-      body = (res as any).response.body
+    if ((res as any).response && (res as any).response.body) {
+      body = (res as any).response.body;
     }
-    if((res as any).response && (res as any).response.status){
-      status = (res as any).response.status
+    if ((res as any).response && (res as any).response.status) {
+      status = (res as any).response.status;
     }
-    
-    log.status = status
-    log.response = JSON.stringify(body)
-    log.date = new Date()
 
-    logRepository.save(log)
+    log.status = status;
+    log.response = JSON.stringify(body);
+    log.date = new Date();
 
-    return res.status(status).send(body) 
+    logRepository.save(log);
 
+    return next();
   },
-]
+];
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 require("dotenv").config();
 import express from "express";
 import morgan from "morgan";
+import fs from "fs";
+import https from "https";
 import Router from "./api/routes/Router";
 import { initDatabase } from "./config/database";
 import { logHandler } from "./api/middleware/logHandler";
@@ -12,13 +14,32 @@ app.use(morgan("tiny"));
 app.use(Router);
 app.use(logHandler);
 
+app.use((err, req, res, next) => {
+  console.log(err);
+
+  res.status(500).json({
+    error: {
+      message: err.message,
+    },
+  });
+});
 
 const port = process.env.PORT || 3001;
 
 initDatabase()
   .then(() => {
-    app.listen(port, () => {
-      console.log("Escuchando Puerto: " + port);
-    });
+    if (process.env.ENV.toLowerCase() === "dev") {
+      const options = {
+        key: fs.readFileSync("key.pem", "utf-8"),
+        cert: fs.readFileSync("cert.pem", "utf-8"),
+      };
+      https.createServer(options, app).listen(port, () => {
+        console.log(`server https runs on port ${port}`);
+      });
+    } else {
+      app.listen(port, () => {
+        console.log("Escuchando Puerto: " + port);
+      });
+    }
   })
   .catch((err) => console.log(err));
